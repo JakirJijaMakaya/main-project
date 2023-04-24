0b81be72229017654e13ae44b5d5956b2815e50f
JuliaMukh <mukhina_ur@mail.ru>
2023-04-24 14:07:32 +0500

    lysya1

--- index.php
+++ index.php
@@ -1,52 +1,20 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Клиент-серверное приложение</title>
-</head>
-<body>
-<form action="insertStudent.php" method="post">
-    <input type="text" name="fname" id="fname" placeholder="введите имя" require><br>
-    <input type="text" name="lname" id="lname" placeholder="введите фамилию" require><br>
-    <input type="number" name="age" id="age" placeholder="введите имя" require><br>
-    <input type="radio" name="gender" id="m" value="m" checked>
-    <label for="m">мужской</label>
-    <input type="radio" name="gender" id="f" value="f">
-    <label for="m">женский</label>
-    <input type="submit" value="добавить">
-</form>
 <?php
+header("Content-Type:text/html; charset=UTF-8");
 
-require_once("config.php");
+require_once("api/config.php");
+require_once("api/core.php");
 
-//соединение с БД
-$connect = new mysqli(HOST, USER, PASSWORD, DB);
-if($connect->connect_error){
-    exit("Ошибка подключения к БД: ".$connect->connect_error);
-}
-//установить кодировку
-$connect->set_charset("utf8");
-
-//код запроса
-$sql = "SELECT * FROM `students`";
-
-//выполнить запрос
-$result = $connect->query($sql);
-
-//вести результаты запроса на страницу
-while ($row = $result->fetch_assoc()){
-    echo "<div>
-            $row[lname], $row[fname], $row[gender], $row[age]
-        </div>";
+if(file_exists("api/main.php")){//проверяем существование файла
+    include("api/main.php");// то подключаем
+    if (class_exists("Main")){//если сущ класс
+        $obj = new Main;// создаем объект от класса Main
+        $obj->get_body();
+    } else {
+        exit("<p>неверный класс</p>");
+    }
+   
 }
 
-
-
-
-
-?>
-    
-</body>
-</html>
+else {
+    exit("<p>неверный путь</p>");
+}
